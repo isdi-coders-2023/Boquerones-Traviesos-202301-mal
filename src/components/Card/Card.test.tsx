6e2991b94ea9896beff97b2b958e6f447c8ec190
Abel <abelguevaralanda@yahoo.es>
2023-02-14 17:01:06 +0100

    add test card component

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,1 +1,12 @@
-export {};
+import { render, screen } from '@testing-library/react';
+import DisneyCard from './Card';
+
+describe('Given an card component', () => {
+  test("When rendering the character's name and image is displayed", () => {
+    render(<DisneyCard />);
+    const titleElement = screen.getByText('Mickey Mouse');
+    const imageElemnt = screen.getByRole('img');
+    expect(titleElement).toBeInTheDocument();
+    expect(imageElemnt).toBeInTheDocument();
+  });
+});
