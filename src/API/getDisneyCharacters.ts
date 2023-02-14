b08f136d3a208385855990b0b6113b170f1898dc
claramarsango <claraxmarsango@gmail.com>
2023-02-14 15:31:50 +0100

    create function to get characters from api

---
+++ src/API/getDisneyCharacters.ts
@@ -0,0 +1,27 @@
+import Character from '../Store/types/character';
+
+const charactersList: Character[] = [];
+
+const getDisneyCharacters = async () => {
+  const response = await fetch(`https://api.disneyapi.dev/character?films`);
+  const characters = await response.json();
+  const charactersInfo = characters.data;
+
+  for (let characterObject of charactersInfo) {
+    const { _id, name, imageUrl, films, tvShows } = characterObject;
+
+    const character: Character = {
+      id: _id,
+      name: name,
+      imageUrl: imageUrl,
+      films: films,
+      tvShows: tvShows,
+    };
+    charactersList.push(character);
+  }
+  console.log(charactersList);
+};
+
+getDisneyCharacters();
+
+export default charactersList;
