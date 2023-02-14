3e1d466d30da6137d3097d8e93c8f39edbe0ca4e
Abel <abelguevaralanda@yahoo.es>
2023-02-14 21:23:43 +0100

    introduce custom hook in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { get } from 'http';
+import React, { useEffect } from 'react';
 
 import './App.css';
 import DisneyCard from './components/Card/Card';
+import useGetCharactersList from './components/hooks/useGetCharactersList/useGetCharactersList';
 
 function App() {
+  const { characters, getCharactersList } = useGetCharactersList();
+
+  useEffect(() => {
+    getCharactersList();
+  }, []);
   return (
     <>
       <DisneyCard />
