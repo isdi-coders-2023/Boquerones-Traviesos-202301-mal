e1c53f7994dc677549f04d593590202bf4d6510c
Abel <abelguevaralanda@yahoo.es>
2023-02-15 20:48:39 +0100

    apply props to acces characters properties and test card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro';
 import './Card.css';
-import picture from './mickey-mouse.png';
+import { FC } from 'react';
+import Character from '../../Store/types/character';
 
-const DisneyCard = () => {
+interface DisneyCardProps {
+  character: Character;
+}
+const DisneyCard: FC<DisneyCardProps> = ({ character }) => {
   return (
     <li className="card-container">
       <div className="img-container">
-        <img className="img-container__img" src={picture} alt="foto"></img>
+        <img
+          className="img-container__img"
+          src={character.imageUrl}
+          alt=""
+        ></img>
       </div>
-      <p className="card__character-title">Mickey Mouse</p>
-      <ul className="card__character-films-list">
-        <p className="character-films__title">Films</p>
-        <li className="character-films__movies">"Who Framed Roger Rabbit"</li>
-      </ul>
-      <ul className="card__character-show-list">
-        <p className="character-show-list__title">TV Shows</p>
-        <li className="character-show-list__series">
-          "Who Framed Roger Rabbit"
-        </li>
-      </ul>
+      <p className="card__character-title">{character.name}</p>
       <div className="button-container">
         <button className="button-container__heart">
           <FontAwesomeIcon icon={regular('heart')} />
