de3612b2954e7b7f93d248c9bd7a4c8552cf0b3a
Abel <abelguevaralanda@yahoo.es>
2023-02-15 15:50:50 +0100

    changes in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { get } from 'http';
-import React, { useEffect } from 'react';
-
 import './App.css';
 import DisneyCard from './components/Card/Card';
-import useGetCharactersList from './components/hooks/useGetCharactersList/useGetCharactersList';
 
 function App() {
-  const { characters, getCharactersList } = useGetCharactersList();
-
-  useEffect(() => {
-    getCharactersList();
-  }, []);
   return (
     <>
       <DisneyCard />
