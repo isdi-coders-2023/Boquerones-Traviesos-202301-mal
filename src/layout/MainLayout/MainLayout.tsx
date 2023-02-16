3bc543a73939a9e31540282b25a6e4e1c228395e
claramarsango <claraxmarsango@gmail.com>
2023-02-16 16:06:51 +0100

    create main layout

---
+++ src/layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default MainLayout;
