efc181ea804d4dd6a5a581f46f9aa41b3a4fd14c
claramarsango <claraxmarsango@gmail.com>
2023-02-16 16:27:15 +0100

    add style to home and detail card title pages

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,7 +1,9 @@
+import './Detail.css';
+
 const Detail = () => {
   return (
     <>
-      <h1>Detail card</h1>
+      <h1 className="detail__title">Detail card</h1>
     </>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import CharactersList from '../../components/CardList/CardList';
 import useGetCharactersList from '../../components/hooks/useGetCharactersList/useGetCharactersList';
+import './Home.css';
 
 const Home = () => {
   const { characters, getCharactersList } = useGetCharactersList();
@@ -10,7 +11,7 @@ const Home = () => {
   }, []);
   return (
     <>
-      <h2>Characters</h2>
+      <h2 className="home__title">Characters</h2>
       <CharactersList characterList={characters} />
     </>
   );
