a615e064f7c3e3b481b6a9a7880046e10f3993a0
claramarsango <claraxmarsango@gmail.com>
2023-02-16 16:10:36 +0100

    import and implement router memory to card list test

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import getDisneyCharacters from '../../API/getDisneyCharacters';
 import { server } from '../../mocks/server';
 import CharactersList from './CardList';
@@ -9,7 +10,11 @@ describe('Given an cardList component', () => {
   afterAll(() => server.close());
   test('When rendering the list then it should display a character card', async () => {
     const characters = await getDisneyCharacters();
-    render(<CharactersList characterList={characters} />);
+    render(
+      <MemoryRouter>
+        <CharactersList characterList={characters} />
+      </MemoryRouter>
+    );
     const liElement = screen.getByRole('listitem');
     expect(liElement).toBeInTheDocument();
   });
