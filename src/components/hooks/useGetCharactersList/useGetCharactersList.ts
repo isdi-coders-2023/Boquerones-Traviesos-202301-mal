35ebe08c3a6e06dd5ea8d695f1d32133eccf690b
Abel <abelguevaralanda@yahoo.es>
2023-02-17 19:36:47 +0100

    update custom hook for datail page

--- src/components/hooks/useGetCharactersList/useGetCharactersList.ts
+++ src/components/hooks/useGetCharactersList/useGetCharactersList.ts
@@ -1,23 +1,35 @@
 import { useCallback, useContext } from 'react';
+import getDisneyCharaterID from '../../../API/getDisneyCharacterId';
 import getDisneyCharacters from '../../../API/getDisneyCharacters';
 import CharacterContext from '../../../Store/contexts/disneyApp.context';
-import { ActionTypes } from '../../../Store/types/Action';
-
+import DisneyAction, { ActionTypes } from '../../../Store/types/Action';
+import { useParams } from 'react-router-dom';
 const useGetCharactersList = () => {
   const { characters, dispatch } = useContext(CharacterContext);
-
+  const { _id } = useParams();
 
   const getCharactersList = useCallback(async () => {
     const characterList = await getDisneyCharacters();
 
-    const showCharactersAction = {
+    const showCharactersAction: DisneyAction = {
       type: ActionTypes.SHOW_CHARACTERS,
       payload: characterList,
     };
     dispatch(showCharactersAction);
   }, [dispatch]);
 
-  return { characters, getCharactersList };
+  const getCharacterDetail = useCallback(async () => {
+    const character = await getDisneyCharaterID(_id);
+
+    const showCharacterDetailAction: DisneyAction = {
+      type: ActionTypes.SHOW_CHARACTER_DETAILS,
+      payload: character,
+    };
+
+    dispatch(showCharacterDetailAction);
+  }, [_id, dispatch]);
+
+  return { characters, getCharactersList, getCharacterDetail };
 };
 
 export default useGetCharactersList;
