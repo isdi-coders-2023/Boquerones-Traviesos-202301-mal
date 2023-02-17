26ed4ab3abe36e0ac6c7fd1bc6ce98b25565fa0c
Abel <abelguevaralanda@yahoo.es>
2023-02-17 16:04:38 +0100

    update detail page

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,10 +1,32 @@
+import DisneyDetailCard from '../../components/DetailCard/DetailCard';
 import './Detail.css';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import getDisneyCharaterID from '../../API/getDisneyCharacterId';
 
 const Detail = () => {
+  const [character, updateCharacter] = useState<any>({});
+  const { _id } = useParams();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getDisneyCharacter = async () => {
+      const character = await getDisneyCharaterID(_id);
+      updateCharacter(character);
+      setLoading(!loading);
+    };
+    getDisneyCharacter();
+  }, [_id]);
+
   return (
     <>
-      <h1 className="detail__title">Detail card</h1>
+      {loading ? (
+        <p>CARGANDO...</p>
+      ) : (
+        <DisneyDetailCard character={character} />
+      )}
     </>
   );
 };
+
 export default Detail;
