81bf50b791dfb347de668958ec74324441ef2c46
carlitin <dreamshavana@gmail.com>
2023-02-17 15:36:52 +0100

    add navigation component to header

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import Navigation from '../../components/Navigation/Navigation';
+import '../../components/Navigation/Navigation.css';
+import { MemoryRouter } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
     <>
-      <header className="header">DisneyCloud</header>
+      <header className="header">
+        <Navigation></Navigation>
+      </header>
       <main>
         <Outlet />
       </main>
