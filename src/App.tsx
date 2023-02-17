414911ae7571ab901c0743e0d60da3166b7c67de
carlitin <dreamshavana@gmail.com>
2023-02-17 11:01:10 +0100

    delete navigation component from app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 
 import './components/Navigation/Navigation.css';
 import './App.css';
-import Navigation from './components/Navigation/Navigation';
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation></Navigation>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
