f6c35c5cb3c30694f43f6e53c443003b81dc97ca
carlitin <dreamshavana@gmail.com>
2023-02-17 14:04:43 +0100

    add favourite page

---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,12 @@
+const Favourites = () => {
+  return (
+    <>
+      <h2>Favourites</h2>
+      <div>
+        <p>You haven't added any characters to your list of favourites yet!</p>
+      </div>
+    </>
+  );
+};
+
+export default Favourites;
