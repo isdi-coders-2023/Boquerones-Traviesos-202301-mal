8c2091194d786a7f9794b38e555064bea054d3cc
carlitin <dreamshavana@gmail.com>
2023-02-17 18:34:25 +0100

    delete import memory router

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from '../../components/Navigation/Navigation';
 import '../../components/Navigation/Navigation.css';
-import { MemoryRouter } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
