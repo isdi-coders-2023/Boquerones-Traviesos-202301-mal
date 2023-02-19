2b767231cb654c78b809359772dc9580d5bde567
claramarsango <claraxmarsango@gmail.com>
2023-02-19 14:14:18 +0100

    add pagination use cases to custom hook and reducer testing

--- src/Store/reducer/disneyReducer.test.ts
+++ src/Store/reducer/disneyReducer.test.ts
@@ -1,15 +1,106 @@
-import { ActionTypes } from '../types/Action';
-import Character from '../types/character';
+import DisneyAction, { ActionTypes } from '../types/Action';
+import DataStructure from '../types/data';
+import disneyReducer from './disneyReducer';
 import charactersReducer from './disneyReducer';
 
-describe('Given a default case for my switch action.type', () => {
-  test('When has not change by function useReducer, the state is not change', () => {
-    const previousCharacters: Character[] = [];
+describe('Given the app reducer', () => {
+  const mockDefault: DataStructure = {
+    characters: [],
+    favourites: [],
+    homeCurrentPage: 5,
+    favouritesCurrentPage: 5,
+    homeOffset: 200,
+    favouritesOffset: 200,
+  };
+  const mockMinimumPages: DataStructure = {
+    characters: [],
+    favourites: [],
+    homeCurrentPage: 1,
+    favouritesCurrentPage: 1,
+    homeOffset: 0,
+    favouritesOffset: 0,
+  };
+
+  const mockMaximumPages: DataStructure = {
+    characters: [],
+    favourites: [],
+    homeCurrentPage: 34,
+    favouritesCurrentPage: 1,
+    homeOffset: 0,
+    favouritesOffset: 0,
+  };
+
+  test('When the useReducer function makes no changes, then the state should not change', () => {
+    const previousCharacters: DataStructure = {
+      characters: [],
+      favourites: [],
+      homeCurrentPage: 1,
+      favouritesCurrentPage: 1,
+      homeOffset: 0,
+      favouritesOffset: 0,
+    };
     const updateAction = {
       type: 'default' as unknown as ActionTypes,
       payload: [],
     };
     const updateState = charactersReducer(previousCharacters, updateAction);
-    expect(updateState).toEqual([]);
+    expect(updateState).toEqual({
+      characters: [],
+      favourites: [],
+      homeCurrentPage: 1,
+      favouritesCurrentPage: 1,
+      homeOffset: 0,
+      favouritesOffset: 0,
+    });
+  });
+
+  test('When the user triggers a previous Home page function on the first page, the Home pagination number should stay the same', () => {
+    const previousPageHome: DisneyAction = {
+      type: ActionTypes.HOME_PREVIOUS_PAGE,
+    };
+
+    const executeNextPageHome = disneyReducer(
+      mockMinimumPages,
+      previousPageHome
+    );
+    expect(executeNextPageHome.homeCurrentPage).toBe(
+      mockMinimumPages.homeCurrentPage
+    );
+  });
+
+  test('When the user triggers a next Home page function on the last page, the Home pagination number should stay the same', () => {
+    const nextPageHome: DisneyAction = {
+      type: ActionTypes.HOME_NEXT_PAGE,
+    };
+
+    const executeNextPageHome = disneyReducer(mockMaximumPages, nextPageHome);
+    expect(executeNextPageHome.homeCurrentPage).toBe(
+      mockMaximumPages.homeCurrentPage
+    );
+  });
+
+  test('When the user triggers a previous Favourites page function on a page that is not the first, the Favourites pagination number should decrease by 1', () => {
+    const previousPageFavourites: DisneyAction = {
+      type: ActionTypes.FAVOURITES_PREVIOUS_PAGE,
+    };
+
+    const executeNextPageHome = disneyReducer(
+      mockDefault,
+      previousPageFavourites
+    );
+    expect(executeNextPageHome.favouritesCurrentPage).toBe(
+      mockDefault.favouritesCurrentPage - 1
+    );
+  });
+
+  test('When the user triggers a next Favourites page function, the Favourites pagination number should increase by 1', () => {
+    const nextPageFavourites: DisneyAction = {
+      type: ActionTypes.FAVOURITES_NEXT_PAGE,
+    };
+
+    const executeNextPageHome = disneyReducer(mockDefault, nextPageFavourites);
+    expect(executeNextPageHome.favouritesCurrentPage).toBe(
+      mockDefault.favouritesCurrentPage + 1
+    );
   });
 });
