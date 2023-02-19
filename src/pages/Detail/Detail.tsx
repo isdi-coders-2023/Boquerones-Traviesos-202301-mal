a9626adb478d511547c6783b6c9823212a3e8051
Abel <abelguevaralanda@yahoo.es>
2023-02-19 21:22:05 +0100

    fix detail page

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,25 +1,18 @@
 import DisneyDetailCard from '../../components/DetailCard/DetailCard';
 import './Detail.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useGetCharactersList from '../../components/hooks/useGetCharactersList/useGetCharactersList';
 
 const Detail = () => {
   const { data, getCharacterDetail } = useGetCharactersList();
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    //   setLoading(!loading);
     getCharacterDetail();
   }, [getCharacterDetail]);
 
   return (
     <>
-      {
-        /* {loading ? (
-        <p>CARGANDO...</p>
-      ) : ( */
-        <DisneyDetailCard character={data.character} />
-      }
+      <DisneyDetailCard character={data.character} />
     </>
   );
 };
