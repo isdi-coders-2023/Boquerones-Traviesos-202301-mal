680e581071d48a907257203811198f95d03c3d03
claramarsango <claraxmarsango@gmail.com>
2023-02-19 14:13:18 +0100

    add pagination functions to custom hook

--- src/components/hooks/useGetCharactersList/useGetCharactersList.ts
+++ src/components/hooks/useGetCharactersList/useGetCharactersList.ts
@@ -1,23 +1,56 @@
 import { useCallback, useContext } from 'react';
 import getDisneyCharacters from '../../../API/getDisneyCharacters';
 import CharacterContext from '../../../Store/contexts/disneyApp.context';
-import { ActionTypes } from '../../../Store/types/Action';
+import DisneyAction, { ActionTypes } from '../../../Store/types/Action';
 
 const useGetCharactersList = () => {
-  const { characters, dispatch } = useContext(CharacterContext);
-
+  const { data, dispatch } = useContext(CharacterContext);
 
   const getCharactersList = useCallback(async () => {
-    const characterList = await getDisneyCharacters();
+    const allCharacters = await getDisneyCharacters();
+    const newPageCharacters = allCharacters.slice(
+      data.homeOffset,
+      data.homeOffset + 100
+    );
 
-    const showCharactersAction = {
+    const showCharactersAction: DisneyAction = {
       type: ActionTypes.SHOW_CHARACTERS,
-      payload: characterList,
+      payload: newPageCharacters,
     };
-    dispatch(showCharactersAction);
-  }, [dispatch]);
 
-  return { characters, getCharactersList };
+    dispatch(showCharactersAction);
+  }, [data.homeOffset, dispatch]);
+
+  const homePagination = (direction: 'next' | 'prev') => {
+    const nextPageAction: DisneyAction = {
+      type: ActionTypes.HOME_NEXT_PAGE,
+    };
+    const previousPageAction: DisneyAction = {
+      type: ActionTypes.HOME_PREVIOUS_PAGE,
+    };
+    direction === 'next'
+      ? dispatch(nextPageAction)
+      : dispatch(previousPageAction);
+  };
+
+  const favouritesPagination = (direction: 'next' | 'prev') => {
+    const nextPageAction: DisneyAction = {
+      type: ActionTypes.FAVOURITES_NEXT_PAGE,
+    };
+    const previousPageAction: DisneyAction = {
+      type: ActionTypes.FAVOURITES_PREVIOUS_PAGE,
+    };
+    direction === 'next'
+      ? dispatch(nextPageAction)
+      : dispatch(previousPageAction);
+  };
+
+  return {
+    data,
+    getCharactersList,
+    homePagination,
+    favouritesPagination,
+  };
 };
 
 export default useGetCharactersList;
