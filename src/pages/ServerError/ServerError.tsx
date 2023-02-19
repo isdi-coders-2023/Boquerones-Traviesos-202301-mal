3d30bea03ed658145dbfa387f63c5be3e7ca7035
carlitin <dreamshavana@gmail.com>
2023-02-19 10:24:11 +0100

    add ServerError page

---
+++ src/pages/ServerError/ServerError.tsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import './ServerError.css';
+
+const ServerError = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="image_container">
+          <img
+            className="image_grumpy"
+            src="/assets/pngwing.png"
+            alt="grumpy character"
+          />
+        </div>
+
+        <div className="error_container">
+          <h2 className="error_title">Oops!</h2>
+
+          <p role={'paragraph'} className="error_title-description">
+            There is a problem with the server.Please try again
+          </p>
+          <Link to={'/home'}>
+            <button className="button_home">Back to Home</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ServerError;
