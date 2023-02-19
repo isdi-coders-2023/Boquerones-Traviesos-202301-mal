9b5cccb149bfa0556a8319b9208d64631f9eabfd
Abel <abelguevaralanda@yahoo.es>
2023-02-19 19:26:21 +0100

    add test detail page

--- src/pages/Detail/Detail.test.tsx
+++ src/pages/Detail/Detail.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Detail from './Detail';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Given a Home page,', () => {
   test('When there is a title, then render it on the page', () => {
-    render(<Detail />);
+    render(
+      <MemoryRouter>
+        <Detail />
+      </MemoryRouter>
+    );
     const title = screen.getByRole('heading');
     expect(title).toBeInTheDocument();
   });
