c6cd100b302239463d9a355252cebcf448efeeb8
Abel <abelguevaralanda@yahoo.es>
2023-02-19 19:21:56 +0100

    add detail card with test

--- src/components/DetailCard/DetailCard.test.tsx
+++ src/components/DetailCard/DetailCard.test.tsx
@@ -1,1 +1,24 @@
-export {};
+import { render, screen } from '@testing-library/react';
+import getDisneyCharaterID from '../../API/getDisneyCharacterId';
+import { server } from '../../mocks/server';
+import DisneyDetailCard from './DetailCard';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Given a detail card component', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.restoreHandlers());
+  afterAll(() => server.close());
+  test('When rendering the character name and image is displayed', async () => {
+    const character = await getDisneyCharaterID('25');
+    console.log(character);
+    render(
+      <MemoryRouter>
+        <DisneyDetailCard character={character.data[0]} />
+      </MemoryRouter>
+    );
+    const titleElement = screen.getByRole('heading');
+    const imageElement = screen.getByRole('img');
+    expect(titleElement).toBeInTheDocument();
+    expect(imageElement).toBeInTheDocument();
+  });
+});
--- src/components/DetailCard/DetailCard.tsx
+++ src/components/DetailCard/DetailCard.tsx
@@ -1,33 +1,54 @@
+import { regular, solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FC } from 'react';
 import Character from '../../Store/types/character';
+import './DetailCard.css';
+import { Link } from 'react-router-dom';
 
 interface DetailCardProps {
   character: Character;
 }
 
 const DisneyDetailCard: FC<DetailCardProps> = ({ character }) => {
-  const filmsList = character.films.slice(0, 6);
-  const showsList = character.tvShows.slice(0, 6);
+  const filmsList = character.films.slice(0, 3);
+  const showsList = character.tvShows.slice(0, 3);
   return (
     <div className="detail-container">
-      <h3 className="detail__title">{character.name}</h3>
+      <div className="arrow-container">
+        <Link to={'/'}>
+          <button className="arrow-button">
+            <FontAwesomeIcon icon={solid('arrow-left')} />
+          </button>
+        </Link>
+        <h3 className="detail__title">{character.name}</h3>
+      </div>
+
       <img alt="" className="detail__img" src={character.imageUrl} />
-      <h4 className="detail__title-films">Films</h4>
-      <ul className="detail__list-films">
-        {filmsList.map((film) => (
-          <li className="detail__list-item" key={film}>
-            "{film}"
-          </li>
-        ))}
-      </ul>
-      <h4 className="detail__title-shows">TV Shows</h4>
-      <ul className="detail__list-shows">
-        {showsList.map((show) => (
-          <li className="detail__list-item" key={show}>
-            "{show}"
-          </li>
-        ))}
-      </ul>
+      <div className="films__container">
+        <p className="detail__title-films">Films:</p>
+        <ul className="detail__list-films">
+          {filmsList.map((film) => (
+            <li className="detail__list-item" key={film}>
+              "{film}"
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="shows__container">
+        <p className="detail__title-shows">TV Shows:</p>
+        <ul className="detail__list-shows">
+          {showsList.map((show) => (
+            <li className="detail__list-item" key={show}>
+              "{show}"
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="button__container-heart">
+        <button className="button-heart">
+          <FontAwesomeIcon icon={regular('heart')} />
+        </button>
+      </div>
     </div>
   );
 };
