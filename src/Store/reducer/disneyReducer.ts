13bb8ea92e9a8372848c26eac098d7b38ecb948e
claramarsango <claraxmarsango@gmail.com>
2023-02-19 14:11:27 +0100

    apply data structure to context, provider and reducer

--- src/Store/reducer/disneyReducer.ts
+++ src/Store/reducer/disneyReducer.ts
@@ -1,15 +1,71 @@
 import DisneyAction, { ActionTypes } from '../types/Action';
-import Character from '../types/character';
+import DataStructure from '../types/data';
 
 const charactersReducer = (
-  previousDisney: Character[],
+  previousState: DataStructure,
   action: DisneyAction
-): Character[] => {
+): DataStructure => {
+  const {
+    homeCurrentPage,
+    favouritesCurrentPage,
+    homeOffset,
+    favouritesOffset,
+  } = previousState;
+
   switch (action.type) {
     case ActionTypes.SHOW_CHARACTERS:
-      return [...action.payload];
+      return { ...previousState, characters: [...action.payload] };
+
+    case ActionTypes.HOME_NEXT_PAGE:
+      return homeCurrentPage < 34
+        ? {
+            ...previousState,
+            homeCurrentPage: homeCurrentPage + 1,
+            homeOffset: homeOffset + 100,
+          }
+        : {
+            ...previousState,
+            homeCurrentPage: homeCurrentPage,
+            homeOffset: homeOffset,
+          };
+    case ActionTypes.HOME_PREVIOUS_PAGE:
+      return homeCurrentPage > 1
+        ? {
+            ...previousState,
+            homeCurrentPage: homeCurrentPage - 1,
+            homeOffset: homeOffset - 100,
+          }
+        : {
+            ...previousState,
+            homeCurrentPage: homeCurrentPage,
+            homeOffset: homeOffset,
+          };
+    case ActionTypes.FAVOURITES_NEXT_PAGE:
+      return favouritesOffset > 100
+        ? {
+            ...previousState,
+            favouritesCurrentPage: favouritesCurrentPage + 1,
+            favouritesOffset: favouritesOffset + 100,
+          }
+        : {
+            ...previousState,
+            favouritesCurrentPage: favouritesCurrentPage,
+            favouritesOffset: favouritesOffset,
+          };
+    case ActionTypes.FAVOURITES_PREVIOUS_PAGE:
+      return favouritesCurrentPage > 1
+        ? {
+            ...previousState,
+            favouritesCurrentPage: favouritesCurrentPage - 1,
+            favouritesOffset: favouritesOffset - 100,
+          }
+        : {
+            ...previousState,
+            favouritesCurrentPage: favouritesCurrentPage,
+            favouritesOffset: favouritesOffset,
+          };
     default:
-      return [...previousDisney];
+      return { ...previousState };
   }
 };
 
